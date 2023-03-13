9485b308969bc7a1d9c699435fc1d96377962cc7
MathMattar <mathmattar@hotmail.com>
2023-03-13 20:30:17 -0300

    Change thema

---
+++ assets/js/switchThema.js
@@ -0,0 +1,16 @@
+const switchButton = document.getElementById("switch");
+const iconThema = document.getElementById("iconThema");
+
+switchButton.addEventListener("click", () => {
+
+  document.body.classList.toggle("switch");
+  document.body.classList.toggle("light-mode")
+
+  if (document.body.classList.contains("switch")) {
+    iconThema.src = "./assets/images/icon-moon.svg";
+    iconThema.alt = "Moon icon";
+  } else {
+    iconThema.src = "./assets/images/icon-sun.svg";
+    iconThema.alt = "Sun icon";
+  }
+});
