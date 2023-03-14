78802003b248ea283fbafbd6c6729d20c2c081df
MathMattar <mathmattar@hotmail.com>
2023-03-14 15:49:01 -0300

    Root file to-do list

--- assets/js/toDoList.js
+++ assets/js/toDoList.js
@@ -1,48 +1,16 @@
-let toDoList = [];
+import createTask from "./newTask.js";
 
 const formTask = document.getElementById("formTask");
-const newTask = document.getElementById("newTask");
-const list = document.getElementById("toDoList");
+const insertTask = document.getElementById("insertTask");
 
 formTask.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  if (newTask.value === "") {
+  if (insertTask.value === "") {
     e.preventDefault();
   } else {
     createTask();
-    showTask();
-
-    newTask.focus();
+    insertTask.focus();
+    insertTask.value = "";
   }
-});
-
-function createTask() {
-  newTaskValue = newTask.value;
-  toDoList.push({ item: newTaskValue });
-  newTask.value = "";
-}
-
-function showTask() {
-  list.innerHTML = "";
-
-  toDoList.forEach((e, i) => {
-    list.innerHTML += `
-    <li class="to-do__item" data-value="${i}">
-        <input type="checkbox" class="to-do__button">
-        <span class="to-do__task">${e.item}</span>
-        <button type="button" class="to-do__delete"">
-            <img src="./assets/images/icon-cross.svg" alt="Close icon" class="to-do__delete--icon">
-        </button>
-    </li>
-    <hr>
-    `;
-  });
-}
-
-const buttonsCompleteTask = document.querySelectorAll(".to-do__button");
-const task = document.querySelectorAll(".to-do__task");
-
-buttonsCompleteTask.forEach((button) => {
-  button.addEventListener("change", (e) => {});
 });
