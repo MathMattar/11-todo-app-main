ee60d8e9177bf6b5e69ac06325619d391a0cb7de
MathMattar <mathmattar@hotmail.com>
2023-03-16 15:07:00 -0300

    Implemented filter functionality

--- assets/js/newTasks.js
+++ assets/js/newTasks.js
@@ -1,20 +1,23 @@
 import { completeTask, updateTaskCount, excludeTask } from "./tools.js";
 
+// Obtém o elemento da lista de tarefas do HTML e o elemento do campo de entrada do HTML
 const list = document.getElementById("to-do-list");
 const insertTask = document.getElementById("insert-task");
 
+// Criar uma nova tarefa
 export function newTask() {
   const taskText = insertTask.value;
-  const toDo = JSON.parse(localStorage.getItem("toDoList")) || [];
+  const toDo = JSON.parse(localStorage.getItem("toDoList")) || []; // Obtém a lista de tarefas armazenada no localStorage ou cria uma lista vazia
 
   let index = toDo.length;
 
+  // Cria uma nova tarefa com o texto da entrada e um ID único
   const newTask = {
     id: index,
     toDo: taskText,
     completed: false,
   };
-
+  // Adiciona a nova tarefa na lista de tarefas e atualiza o localStorage
   toDo.push(newTask);
   localStorage.setItem("toDoList", JSON.stringify(toDo));
   index++;
@@ -22,24 +25,37 @@ export function newTask() {
   renderToDoList();
 }
 
-export function renderToDoList() {
+// Renderizar a lista de tarefas
+export function renderToDoList(filterOption = "all") {
   const toDo = JSON.parse(localStorage.getItem("toDoList")) || [];
+
+  // Lista vazia para armazenas as tarefas filtradas
+  let filteredToDo = [];
+
+  // Filtrar as tarefas com base na opção de filtro selecionado
+  if (filterOption === "all") {
+    filteredToDo = toDo;
+  } else if (filterOption === "active") {
+    filteredToDo = toDo.filter((task) => !task.completed);
+  } else if (filterOption === "completed") {
+    filteredToDo = toDo.filter((task) => task.completed);
+  }
 
   list.innerHTML = "";
 
-  toDo.forEach((task, index) => {
+  filteredToDo.forEach((task, index) => {
     const taskItem = document.createElement("li");
     taskItem.classList.add("to-do__item");
     taskItem.setAttribute("data-value", index);
 
     taskItem.innerHTML = `
-    <input type="checkbox" class="to-do__complete" ${
-      task.completed ? "checked" : ""
-    }>
-    <span class="to-do__task">${task.toDo}</span>
-    <button type="button" class="to-do__delete">
+      <input type="checkbox" class="to-do__complete" ${
+        task.completed ? "checked" : ""
+      }>
+      <span class="to-do__task">${task.toDo}</span>
+      <button type="button" class="to-do__delete">
         <img src="./assets/images/icon-cross.svg" alt="Close icon" class="to-do__delete--icon">
-    </button>`;
+      </button>`;
 
     list.appendChild(taskItem);
   });
--- assets/js/tools.js
+++ assets/js/tools.js
@@ -1,14 +1,16 @@
 import { renderToDoList } from "./newTasks.js";
 
+// Completar a tarefa
 export function completeTask() {
   const completeButtons = document.querySelectorAll(".to-do__complete");
 
   completeButtons.forEach((completeButton) => {
     completeButton.addEventListener("change", (e) => {
-      const index = completeButton.parentNode.getAttribute("data-value");
+      const index = completeButton.parentNode.getAttribute("data-value"); // Pega o ID da tarefa a partir do atributo data-value do pai do botão
       const toDo = JSON.parse(localStorage.getItem("toDoList")) || [];
-      const todoIndex = toDo.findIndex((todo) => todo.id === Number(index));
+      const todoIndex = toDo.findIndex((todo) => todo.id === Number(index)); // Busca o índice da tarefa a partir do ID
 
+      // Se a tarefa existe no array, atualiza o status
       if (todoIndex > -1) {
         const todo = toDo[todoIndex];
         todo.completed = completeButton.checked;
@@ -21,6 +23,7 @@ export function completeTask() {
   });
 }
 
+// Excluir a tarefa
 export function excludeTask() {
   const deleteButtons = document.querySelectorAll(".to-do__delete");
 
@@ -28,12 +31,12 @@ export function excludeTask() {
     deleteButton.addEventListener("click", (e) => {
       e.preventDefault();
 
-      const index = deleteButton.parentNode.getAttribute("data-value"); // pega o ID a partir do atributo data-value do pai
+      const index = deleteButton.parentNode.getAttribute("data-value"); // Pega o ID a partir do atributo data-value do pai
       const toDo = JSON.parse(localStorage.getItem("toDoList")) || [];
-      const filterToDo = toDo.filter((task) => task.id != index);
+      const filterToDo = toDo.filter((task) => task.id != index); // Filtra as tarefas que não possuem o ID selecionado e salva no localStorage
       localStorage.setItem("toDoList", JSON.stringify(filterToDo));
 
-      // atualiza o ID dos itens restantes no localStorage
+      // Atualiza o ID dos itens restantes no localStorage
       filterToDo.forEach((task, i) => {
         task.id = i;
       });
@@ -45,11 +48,14 @@ export function excludeTask() {
   });
 }
 
+// Limpar tarefas concluídas
 export function clearCompleted() {
   const clearButton = document.getElementById("clear-complete");
 
   clearButton.addEventListener("click", () => {
     const toDo = JSON.parse(localStorage.getItem("toDoList")) || [];
+
+    // Filtra as tarefas concluídas e incompletas e salva apenas as incompletas no localStorage
     const completedTasks = toDo.filter((task) => task.completed);
     const incompleteTasks = toDo.filter((task) => !task.completed);
 
@@ -58,6 +64,7 @@ export function clearCompleted() {
     renderToDoList();
     updateTaskCount();
 
+    // Atualiza o ID das tarefas restantes no localStorage
     incompleteTasks.forEach((task, i) => {
       task.id = i;
     });
@@ -66,7 +73,8 @@ export function clearCompleted() {
   });
 }
 
-export function updateTaskCount() {
+// Atualizar o contador de taregas incompletas
+export function updateTaskCount(filter) {
   const taskCount = document.getElementById("task-count");
 
   const toDo = JSON.parse(localStorage.getItem("toDoList")) || [];
@@ -74,3 +82,59 @@ export function updateTaskCount() {
 
   taskCount.innerHTML = `${incompleteTasks.length} items left`;
 }
+
+// Adiciona ouvintes aos botões de filtro
+function filterTasks() {
+  const allTasks = document.getElementById("filter-all");
+  const activeTasks = document.getElementById("filter-active");
+  const completedTasks = document.getElementById("filter-completed");
+
+  allTasks.classList.add("active");
+
+  allTasks.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (
+      activeTasks.classList.contains("active") ||
+      completedTasks.classList.contains("active")
+    ) {
+      activeTasks.classList.remove("active");
+      completedTasks.classList.remove("active");
+      allTasks.classList.add("active");
+    }
+
+    renderToDoList("all");
+  });
+
+  activeTasks.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (
+      allTasks.classList.contains("active") ||
+      completedTasks.classList.contains("active")
+    ) {
+      allTasks.classList.remove("active");
+      completedTasks.classList.remove("active");
+      activeTasks.classList.add("active");
+    }
+
+    renderToDoList("active");
+  });
+
+  completedTasks.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (
+      allTasks.classList.contains("active") ||
+      activeTasks.classList.contains("active")
+    ) {
+      allTasks.classList.remove("active");
+      activeTasks.classList.remove("active");
+      completedTasks.classList.add("active");
+    }
+
+    renderToDoList("completed");
+  });
+}
+
+filterTasks();
