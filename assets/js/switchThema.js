6781025158875d805ce76a347de388fb90f5a014
MathMattar <mathmattar@hotmail.com>
2023-03-16 15:06:29 -0300

    Change the theme

--- assets/js/switchThema.js
+++ assets/js/switchThema.js
@@ -1,10 +1,12 @@
 const switchButton = document.getElementById("switch");
 const iconThema = document.getElementById("iconThema");
 
+// Alterar tema
 switchButton.addEventListener("click", () => {
   document.body.classList.toggle("switch");
   document.body.classList.toggle("light-mode");
 
+  // Alterar imagem do botão
   if (document.body.classList.contains("switch")) {
     iconThema.src = "./assets/images/icon-moon.svg";
     iconThema.alt = "Moon icon";
